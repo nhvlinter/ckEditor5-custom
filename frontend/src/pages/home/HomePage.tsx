337efd4446f581251cf9e856d0173175392815c0
nguyenhien0209 <nguyenhien.py.1996@gmail.com>
2021-01-19 11:33:32 +0700

    Add Reset and Save btn

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,8 +1,9 @@
 import React, { FC, ReactNode, ReactElement, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '../../stores';
-import {CKEditor} from '@ckeditor/ckeditor5-react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
 //import CKEditorCustom from '../../components/CKEditor/CKEditorCustom';
+import Button from '@material-ui/core/Button';
 import InlineEditor from '@ckeditor/ckeditor5-build-inline';
 import DIVConversionPlugin from '../../components/CKEditor/DIVConversionPlugin';
 import LabelConversionPlugin from '../../components/CKEditor/LabelConversionPlugin';
@@ -16,42 +17,61 @@ import InputConversionPlugin from '../../components/CKEditor/InputConversionPlug
 import TextAreaConversionPlugin from '../../components/CKEditor/TextAreaConversionPlugin';
 import ATagConversionPlugin from '../../components/CKEditor/ATagConversionPlugin';
 import BtnConversionPlugin from '../../components/CKEditor/BtnConversionPlugin';
+import LiConversionPlugin from '../../components/CKEditor/LiConversionPlugin';
 import styles from "./HomePage.module.scss";
 import { BasicLayout } from '../../layouts/BasicLayout';
+import { makeStyles } from '@material-ui/core/styles';
 
-export const HomePage: FC<{}> = observer(({}) => {
+const useStyles = makeStyles((theme) => ({
+    btn: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+  }));
+
+export const HomePage: FC<{}> = observer(({ }) => {
     const { sCKEditor } = useStore();
-    useEffect(()=>{
+    const classes = useStyles();
+    useEffect(() => {
         sCKEditor.init();
     });
     return (<BasicLayout><div>
-            <h1 style={{marginBottom:50}}>Inline editor</h1>
-            <CKEditor
+        <h1 style={{ marginBottom: 50 }}>Inline editor</h1>
+        <CKEditor
             editor={InlineEditor}
-            config={{extraPlugins:[DIVConversionPlugin,LabelConversionPlugin,SpanConversionPlugin,
-                AnchorConversionPlugin,ImageConversionPlugin,IconConversionPlugin,FormConversionPlugin,
-                UlConversionPlugin,InputConversionPlugin,TextAreaConversionPlugin,ATagConversionPlugin,
-                BtnConversionPlugin]}}
+            config={{
+                extraPlugins: [DIVConversionPlugin, LabelConversionPlugin, SpanConversionPlugin,
+                    AnchorConversionPlugin, ImageConversionPlugin, IconConversionPlugin, FormConversionPlugin,
+                    UlConversionPlugin, InputConversionPlugin, TextAreaConversionPlugin, ATagConversionPlugin,
+                    BtnConversionPlugin]
+            }}
             data={sCKEditor.data}
-            onReady={ editor => {
+            onReady={editor => {
                 // You can store the "editor" and use when it is needed.
-                console.log( 'Editor is ready to use!', editor );
-            } }
-            onChange={ ( event, editor ) => {
+                console.log('Editor is ready to use!', editor);
+            }}
+            onChange={(event, editor) => {
                 const data = editor.getData();
                 sCKEditor.update(data);
-                console.log( { event, editor, data } );
-            } }
-            onBlur={ ( event, editor ) => {
-                console.log( 'Blur.', editor );
-            } }
-            onFocus={ ( event, editor ) => {
-                console.log( 'Focus.', editor );
-            } }
-            />
-            </div>
-            </BasicLayout>
-        );
+                console.log({ event, editor, data });
+            }}
+            onBlur={(event, editor) => {
+                console.log('Blur.', editor);
+            }}
+            onFocus={(event, editor) => {
+                console.log('Focus.', editor);
+            }}
+        />
+        </div>
+        <div className={classes.btn}>
+            <Button variant="contained">Reset</Button>
+            <Button variant="contained" color="primary">
+                Save
+            </Button>
+        </div>
+    </BasicLayout>
+    );
 });
 
 
